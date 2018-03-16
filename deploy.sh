30d1b6bc5ef28e4e7972a977fe031d9f06b1eec3
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-03-16 19:37:47 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+if [ $# -eq 0 ];
+then
+  echo "Usage: ./build_and_deploy.sh \"quoted commit message\""
+  exit
+fi
+
+if [ ! -d "out" ]; then
+  echo "Nothing to deploy. Run ./build.sh before ./deploy.sh"
+  exit
+fi
+
+git checkout gh-pages
+git mv .gitignore.gh.pages .gitignore
+git add .gitignore out
+echo $1
+git commit -m $1
+git push origin gh-pages
+git checkout master
