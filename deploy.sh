e978a402ba970dc78eb32d28db970483f7b726d5
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-03-16 19:47:42 +0100

    Do not require commit message for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,3 @@
-if [ $# -eq 0 ];
-then
-  echo "Usage: ./build_and_deploy.sh \"quoted commit message\""
-  exit
-fi
-
 if [ ! -d "out" ]; then
   echo "Nothing to deploy. Run ./build.sh before ./deploy.sh"
   exit
@@ -12,6 +6,6 @@ fi
 git checkout gh-pages
 git mv .gitignore.gh.pages .gitignore
 git add .gitignore out
-git commit -m $1
+git commit -m "Deploy"
 git push origin gh-pages
 git checkout master
