de3865f4364868dd5f4587982a97dfb05ac86eec
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2018-10-04 12:13:15 +0200

    Split build per target

--- build.sh
+++ build.sh
@@ -1,27 +1,37 @@
-ndk-build
+if [[ -z "${TARGET}" ]]; then
+  TARGET="armv7"
+fi
 
-cp -r libs/armeabi-v7a/libgstreamer_android.so gst-build-armeabi-v7a/
-cp -r $GSTREAMER_ROOT_ANDROID/armv7/lib/pkgconfig gst-build-armeabi-v7a/
-cp -r libs/x86/libgstreamer_android.so gst-build-x86/
-cp -r $GSTREAMER_ROOT_ANDROID/x86/lib/pkgconfig gst-build-x86/
+NDK_APPLICATION_MK="jni/${TARGET}.mk"
+echo "Building for target ${TARGET} with ${NDK_APPLICATION_MK}"
 
-rm -rf libs obj
+ndk-build NDK_APPLICATION_MK=$NDK_APPLICATION_MK
+
+if [ $TARGET = "armv7" ]; then
+  LIB="armeabi-v7a"
+else
+  LIB="x86"
+fi;
+
+GST_LIB="gst-build-${LIB}"
+
+cp -r libs/${LIB}/libgstreamer_android.so ${GST_LIB}
+cp -r $GSTREAMER_ROOT_ANDROID/${TARGET}/lib/pkgconfig ${GST_LIB}
+
+rm -rf libs obj out
 
 mkdir out
 
-for D in gst-build-armeabi-v7a gst-build-x86
-do
-  echo 'Processing '$D
-  cd $D
-  sed -i -e 's?libdir=.*?libdir='`pwd`'?g' pkgconfig/*
-  sed -i -e 's?.* -L${.*?Libs: -L${libdir} -lgstreamer_android?g' pkgconfig/*
-  sed -i -e 's?Libs:.*?Libs: -L${libdir} -lgstreamer_android?g' pkgconfig/*
-  sed -i -e 's?Libs.private.*?Libs.private: -lgstreamer_android?g' pkgconfig/*
-  rm -rf pkgconfig/*-e*
-  cd ..
-  zip -v out/$D.zip $D/* -r
-  rm -rf $D
-done
+echo 'Processing '$GST_LIB
+cd $GST_LIB
+sed -i -e 's?libdir=.*?libdir='`pwd`'?g' pkgconfig/*
+sed -i -e 's?.* -L${.*?Libs: -L${libdir} -lgstreamer_android?g' pkgconfig/*
+sed -i -e 's?Libs:.*?Libs: -L${libdir} -lgstreamer_android?g' pkgconfig/*
+sed -i -e 's?Libs.private.*?Libs.private: -lgstreamer_android?g' pkgconfig/*
+rm -rf pkgconfig/*-e*
+cd ..
+zip -v out/$GST_LIB.zip $GST_LIB/* -r
+rm -rf $GST_LIB
 
 zip -v out/src.zip src -r
 rm -rf src
